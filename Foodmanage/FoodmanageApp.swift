737cb71c70a89f1d6ca9648947ffdb93ad622784
Ciel <1015373877@qq.com>
2025-03-20 23:03:41 +0800

    Initial Commit

---
+++ Foodmanage/FoodmanageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodmanageApp.swift
+//  Foodmanage
+//
+//  Created by 许知知 on 2025/3/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodmanageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodmanageTests/FoodmanageTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodmanageTests.swift
+//  FoodmanageTests
+//
+//  Created by 许知知 on 2025/3/20.
+//
+
+import Testing
+@testable import Foodmanage
+
+struct FoodmanageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
